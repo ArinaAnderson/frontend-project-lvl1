7196b000888645a77ee30088245a2c942c3de114
Marina Zinchenko <iolsoofuolso@gmail.com>
2022-01-04 17:02:13 -0800

    add func from utils module

--- src/games/isEven.js
+++ src/games/isEven.js
@@ -1,12 +1,14 @@
 import setGame from '../index.js';
 
+import { getRandomNumber } from '../utils.js';
+
 const QUESTION_TITLE = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const checkIfEven = (input) => (input % 2 === 0 ? 'yes' : 'no');
-const getRandomNumber = () => Math.floor(Math.random() * 101);
+const getCorrectResult = (input) => (input % 2 === 0 ? 'yes' : 'no');
+const getOption = () => getRandomNumber(0, 100);
 
 const isEven = () => {
-  setGame(QUESTION_TITLE, getRandomNumber, checkIfEven);
+  setGame(QUESTION_TITLE, getOption, getCorrectResult);
 };
 
 export default isEven;
