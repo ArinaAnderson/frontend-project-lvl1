58988d3becdf9b25446634aff1c2bf4f47bdfc63
Marina Zinchenko <iolsoofuolso@gmail.com>
2022-01-08 19:15:11 -0800

    remove arr argument mutation in shuffleArray func

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 export const getRandomNumber = (min, max) => Math.floor(Math.random() * (max + 1 - min)) + min;
 
 export const shuffleArray = (arr) => {
-  const shuffledArr = arr;
+  const shuffledArr = arr.slice(0);
   for (let i = arr.length - 1; i > 0; i -= 1) {
     const randomNum = Math.floor(Math.random() * (i + 1));
     const randomElement = arr[randomNum];
