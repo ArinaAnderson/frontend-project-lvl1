bda6c6e1744c3a0a50f563f1aad3f5d5bbe555c2
Marina Zinchenko <iolsoofuolso@gmail.com>
2022-01-13 23:53:46 -0800

    add buildSequence and buildSequenceString funcs in brain-progression

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -5,29 +5,30 @@ import getRandomNumber from '../utils.js';
 const GAME_TASK = 'What number is missing in the progression?';
 const sequenceLength = 10;
 
+const buildSequence = (idx, sequence, sequenceDif) => {
+  if (idx === sequenceLength) {
+    return sequence;
+  }
+  const newElem = sequence[idx - 1] + sequenceDif;
+  sequence.push(newElem);
+
+  return buildSequence(idx + 1, sequence, sequenceDif);
+};
+
+const buildSequenceString = (arr, hiddenMemberIdx) => {
+  const sequenceMembers = arr.slice(0);
+  sequenceMembers[hiddenMemberIdx] = '..';
+  return sequenceMembers.join(' ');
+};
+
 const setGameParams = () => {
-  let correctVal = null;
   const sequenceDif = getRandomNumber(1, 9);
   const firstMember = getRandomNumber(1, 20);
   const hiddenMemberIdx = getRandomNumber(1, sequenceLength - 1);
 
-  const buildSequence = (idx, acc, sequenceStr) => {
-    if (idx === sequenceLength) {
-      return sequenceStr;
-    }
-    const newElem = acc + sequenceDif;
-    let newSequenceStr;
-    if (idx === hiddenMemberIdx) {
-      newSequenceStr = sequenceStr.concat(' ..');
-      correctVal = newElem;
-    } else {
-      newSequenceStr = sequenceStr.concat(` ${newElem}`);
-    }
-
-    return buildSequence(idx + 1, newElem, newSequenceStr);
-  };
-
-  const questionVal = buildSequence(1, firstMember, `${firstMember}`);
+  const sequenceMembers = buildSequence(1, [firstMember], sequenceDif);
+  const questionVal = buildSequenceString(sequenceMembers, hiddenMemberIdx);
+  const correctVal = sequenceMembers[hiddenMemberIdx];
 
   return { questionVal, correctVal: correctVal.toString() };
 };
