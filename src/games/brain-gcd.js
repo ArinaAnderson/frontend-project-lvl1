608506cf09ab525ff279718b7f3bef936fde2500
Marina Zinchenko <iolsoofuolso@gmail.com>
2022-01-17 22:21:28 -0800

    add Math.min Math.max funcs

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -6,16 +6,13 @@ const GAME_TASK = 'Find the greatest common divisor of given numbers.';
 const numberMaxVal = 100;
 const numberMinVal = 1;
 
-const defineMax = (val1, val2) => (val1 > val2 ? val1 : val2);
-const defineMin = (val1, val2) => (val1 > val2 ? val2 : val1);
-
 const calculateGCD = (x, y) => {
   if (x === y) {
     return x;
   }
   const iter = (val1, val2) => {
-    const max = defineMax(val1, val2);
-    const min = defineMin(val1, val2);
+    const max = Math.max(val1, val2);
+    const min = Math.min(val1, val2);
     if (max % min === 0) {
       return min;
     }
